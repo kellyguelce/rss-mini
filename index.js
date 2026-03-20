e73bd1ac92f46d897ce967694359030ed5d28083
Kelly Guelcé <103383597+kellyguelce@users.noreply.github.com>
2026-03-20 16:18:37 +0000

    Installing  EJS

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+/*
+|----------------------------------
+| App Configuration
+|---------------------------------- */
+app.set('view engine', 'ejs')
+
+/*--------------------------------- */
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(port, () => {
